40c50ab9b2ba3a32e2d4a855f0fbb2eac9101643
Biruk Bizuayehu <birukbizuayehu1167@gmail.com>
2024-04-05 23:05:55 -0900

    add Q7

---
+++ Q7_AW_Marketing_Campaign.sql
@@ -0,0 +1,8 @@
+Marketing Campaign Effectiveness:
+
+Measure the effectiveness of marketing campaigns by analyzing sales data before and after
+the campaigns. Calculate metrics such as sales lift, ROI, and customer acquisition cost.
+
+
+Write SQL queries to extract sales data before and after specific marketing campaigns,
+calculating performance metrics based on campaign periods and customer behavior.
