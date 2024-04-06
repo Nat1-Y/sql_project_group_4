1dd601e14978dfc0b333acb46e2785e9c18289f6
Biruk Bizuayehu <birukbizuayehu1167@gmail.com>
2024-04-05 22:57:44 -0900

    add lag

--- Q1_AW_Sales_Performance_Analysis.sql
+++ Q1_AW_Sales_Performance_Analysis.sql
@@ -6,18 +6,35 @@
  Write SQL queries to retrieve sales data from the AdventureWorks database, grouping by
  product category and aggregating sales amounts over time.
 
-WITH Sales AS
-  (SELECT PPC.Name AS PRODUCT_CATEGORY,
-          YEAR(SOH.OrderDate) AS SALES_YEAR,
-          SUM(SOD.LineTotal) AS TOTAL_AMOUNT
-   FROM Sales.SalesOrderHeader SOH
-   INNER JOIN Sales.SalesOrderDetail SOD ON SOH.SalesOrderID = SOD.SalesOrderID
-   INNER JOIN Production.Product PP ON PP.ProductID = SOD.ProductID
-   INNER JOIN Production.ProductSubcategory PPS ON PPS.ProductSubcategoryID = PP.ProductSubcategoryID
-   INNER JOIN Production.ProductCategory PPC ON PPC.ProductCategoryID = PPS.ProductCategoryID
-   GROUP BY PPC.Name,
-            YEAR(SOH.OrderDate))
-SELECT PRODUCT_CATEGORY,
-       SALES_YEAR,
-       TOTAL_AMOUNT
-FROM Sales
+WITH SalesData AS (
+    SELECT   
+        PC.EnglishProductCategoryName AS PRODUCT_NAME,  
+        DATEPART(YEAR, FRS.OrderDate) AS YEAR,
+        SUM(FRS.SalesAmount) AS TOTAL_SALES,
+        LAG(SUM(FRS.SalesAmount)) 
+      OVER (PARTITION BY PC.EnglishProductCategoryName 
+      ORDER BY DATEPART(YEAR, FRS.OrderDate)) AS PREVIOUS_YEAR_SALES
+    FROM 
+        DimProductCategory PC 
+    INNER JOIN 
+        DimProductSubcategory PSC ON PC.ProductCategoryKey = PSC.ProductCategoryKey
+    INNER JOIN 
+        DimProduct P ON PSC.ProductSubcategoryKey = P.ProductSubcategoryKey
+    INNER JOIN 
+        FactResellerSales FRS ON P.ProductKey = FRS.ProductKey
+    WHERE 
+        FRS.OrderDate BETWEEN '2011-01-01' AND '2013-12-31'
+    GROUP BY 
+        PC.EnglishProductCategoryName, DATEPART(YEAR, FRS.OrderDate)
+)
+SELECT 
+    PRODUCT_NAME,
+    YEAR,
+    TOTAL_SALES,
+    PREVIOUS_YEAR_SALES,
+    YEAR_CHANGE = TOTAL_SALES - PREVIOUS_YEAR_SALES,
+    Percent_Change = CASE WHEN Previous_Year_Sales <> 0 THEN (TOTAL_SALES - PREVIOUS_YEAR_SALES) / PREVIOUS_YEAR_SALES * 100 END
+FROM 
+    SalesData
+ORDER BY 
+    PRODUCT_NAME, YEAR;
